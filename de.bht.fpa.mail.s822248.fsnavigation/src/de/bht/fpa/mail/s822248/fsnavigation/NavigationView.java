3bcfacb843ec60f6aa7592a82bea5c4a4466c695
schubalibre <robertodziuba@googlemail.com>
2015-10-21 18:18:28 +0200

    handler hinzugefügt

--- de.bht.fpa.mail.s822248.fsnavigation/src/de/bht/fpa/mail/s822248/fsnavigation/NavigationView.java
+++ de.bht.fpa.mail.s822248.fsnavigation/src/de/bht/fpa/mail/s822248/fsnavigation/NavigationView.java
@@ -1,25 +1,38 @@
 package de.bht.fpa.mail.s822248.fsnavigation;
 
 import java.io.File;
+import java.util.Observable;
+import java.util.Observer;
 
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-public class NavigationView extends ViewPart{
+import de.bht.fpa.mail.s822248.fsnavigation.handlers.FileObservable;
 
+public class NavigationView extends ViewPart implements Observer{
+
+	FileObservable file;
+	TreeViewer viewer;
 	@Override
 	public void createPartControl(Composite parent) {
-		TreeViewer viewer = new TreeViewer(parent);
+		viewer = new TreeViewer(parent);
 		viewer.setContentProvider(new NsNavigationContentProvider());
 		viewer.setLabelProvider(new FsNavigationLabel());
 		viewer.setInput(new FolderItem(new File(System.getProperty("user.home"))));
+		file = FileObservable.getInstance();
+		file.addObserver(this);
+	}
+	
+	@Override
+	public void setFocus() {
+		
 	}
 
 	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-	
+	public void update(Observable arg0, Object arg1) {
+		String path = (String) arg1;
+		if(path!= null)viewer.setInput(new FolderItem(new File(path)));
 	}
 
 }
---
+++ de.bht.fpa.mail.s822248.fsnavigation/src/de/bht/fpa/mail/s822248/fsnavigation/handlers/FileObservable.java
@@ -0,0 +1,34 @@
+package de.bht.fpa.mail.s822248.fsnavigation.handlers;
+
+import java.util.Observable;
+
+
+public class FileObservable extends Observable {
+	private String path;
+	private  static FileObservable fileObservable=null;
+
+	
+	private FileObservable(){
+		
+	}
+	
+	public static  FileObservable getInstance(){
+		if(fileObservable == null) fileObservable= new FileObservable();
+		return fileObservable;
+	}
+	
+
+
+	public void setPath(String path){
+		setChanged();
+		notifyObservers(path);
+		this.path = path;
+	}
+	
+
+
+	public String getPath(){
+		return path;
+	}
+
+}
