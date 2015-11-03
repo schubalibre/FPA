06cb9464cbef71e14bbae965a65f2233944b163f
Robert Dziuba <robertodziuba@gmail.com>
2015-11-03 19:08:57 +0100

    maillist

---
+++ de.bht.fpa.mail.s822248.maillist/src/de/bht/fpa/mails/s822248/maillist/MailListViewPart.java
@@ -0,0 +1,31 @@
+package de.bht.fpa.mails.s822248.maillist;
+
+import java.util.Collection;
+
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+import de.bht.fpa.mail.s000000.common.mail.testdata.RandomTestDataProvider;
+import de.ralfebert.rcputils.tables.TableViewerBuilder;
+
+public class MailListViewPart  extends ViewPart{
+
+  	private TableViewer tableViewer;
+
+	@Override
+	public void createPartControl(Composite parent) {
+	  Composite tableComposite = new Composite(parent, SWT.NONE);
+    TableViewerBuilder tvb = new MaillistTableViewerBuilder(tableComposite);
+		Collection<Message> messages = new RandomTestDataProvider(50).getMessages();
+		tvb.setInput(messages);
+		this.tableViewer = tvb.getTableViewer();
+	}
+
+	@Override
+	public void setFocus() {
+		this.tableViewer.getControl().setFocus();
+	}
+}
--- de.bht.fpa.mail.s822248.main/src/de/bht/fpa/mail/s822248/main/Perspective.java
+++ de.bht.fpa.mail.s822248.main/src/de/bht/fpa/mail/s822248/main/Perspective.java
@@ -6,7 +6,9 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class Perspective implements IPerspectiveFactory {
 
   public void createInitialLayout(IPageLayout layout) {
-    final float width = 0.7f;
-    layout.addView("de.bht.fpa.mail.s822248.fsnavigation.view1", IPageLayout.LEFT, width, layout.getEditorArea());
+    final float widthTree = 0.3f;
+    final float widthList = 0.7f;
+    layout.addView("de.bht.fpa.mail.s822248.fsnavigation.view1", IPageLayout.LEFT, widthTree, layout.getEditorArea());
+    layout.addView("de.bht.fpa.mails.s822248.maillist.view1", IPageLayout.TOP, widthList, layout.getEditorArea());
   }
 }
